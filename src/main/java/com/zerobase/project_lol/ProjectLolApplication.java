393624f2eca01d73de93cede59bb916409c3f172
pheno <tyon2965@gmail.com>
2023-08-21 17:34:56 +0900

    commit

---
+++ src/main/java/com/zerobase/project_lol/ProjectLolApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.project_lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLolApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectLolApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zerobase/project_lol/ProjectLolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.project_lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
